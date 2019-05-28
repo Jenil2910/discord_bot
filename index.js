cc2949ee8fe5040861ffd02c40cbba0603711e1f
Jenil Mewada <jenil@iitk.ac.in>
2019-05-28 08:35:39 +0530

    fix: tfw no server

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const client = new Discord.Client()
 const fs = require('fs')
 const http = require('http')
 const port = process.env.PORT
+//const port = 4543
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+  console.log(request.url);
+  setInterval(function() {
+    console.log(`Next request coming ${30000}`);
+    http.get("http://thanos-hq.herokuapp.com");
+  }, 30000);
+  response.end('Hello Node.js Server!');
+};
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 });
 
